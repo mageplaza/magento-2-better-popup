8b85a0b51bc4a7c5a03c3650e1ab4f4456a810cb
Ninhkx <ninhkx@mageplaza.com>
2018-07-11 17:26:41 +0700

    [WIP] = Working In Process

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,27 @@
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_BetterPopup
+ * @copyright   Copyright (c) 2017-2018 Mageplaza (https://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+var config = {
+    map: {
+        '*': {
+            'mageplaza/betterpopup_popup': 'Mageplaza_BetterPopup/js/popup',
+            'mageplaza/betterpopup_bioep': 'Mageplaza_BetterPopup/js/bioep'
+        }
+    }
+};
